2152e62b9217da215efd37e6a7058d89bbc1bbbf
Ryan Oberlander <reoberlander2@gmail.com>
2025-01-03 17:04:29 -0600

    start working on fixing GroupDetailPanel (contains errors)

--- lib/ui/groups/GroupDetailPanel.dart
+++ lib/ui/groups/GroupDetailPanel.dart
@@ -104,7 +104,7 @@ class GroupDetailPanel extends StatefulWidget with AnalyticsInfo {
 
   AnalyticsFeature? get _defaultAnalyticsFeature => (group?.researchProject == true) ? AnalyticsFeature.ResearchProject : AnalyticsFeature.Groups;
 
-  static List<_DetailTab> get defaultTabs => [_DetailTab.Events, _DetailTab.Posts,  _DetailTab.Scheduled, _DetailTab.Messages, _DetailTab.Polls]; //TBD extract from Groups BB
+  static List<_DetailTab> get defaultTabs => [_DetailTab.Events, _DetailTab.Posts,  _DetailTab.Scheduled, _DetailTab.Messages, _DetailTab.Polls, _DetailTab.About]; //TBD extract from Groups BB
 }
 
 class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProviderStateMixin implements NotificationsListener {
@@ -117,7 +117,7 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
   String?                 _postId;
 
   final ScrollController _scrollController = ScrollController();
-  late TabController _tabController;
+  TabController?  _tabController;
   PageController? _pageController;
   StreamController _updateController = StreamController.broadcast();
 
@@ -258,9 +258,6 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
       Groups.notifyGroupStatsUpdated,
     ]);
 
-    _tabController = TabController(length: _DetailTab.values.length, initialIndex: _currentTab.index, vsync: this);
-    // _tabController.addListener(_onTabChanged);
-
     _postId = widget.groupPostId;
     _tabs = GroupDetailPanel.defaultTabs;
 
@@ -271,8 +268,7 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
   @override
   void dispose() {
     NotificationService().unsubscribe(this);
-    // _tabController.removeListener(_onTabChanged);
-    _tabController.dispose();
+    _tabController?.dispose();
     _updateController.close();
     _pageController?.dispose();
     super.dispose();
@@ -337,6 +333,29 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
   }
 
   Widget _buildGroupContent() {
+    List<Widget> content = [
+      _buildImageHeader(),
+      _buildGroupInfo()
+    ];
+    if(_isMemberOrAdmin) {
+      content.add(_buildTabs());
+      content.add(_buildViewPager());
+    } else {
+      content.addAll(_buildNonMemberContent());
+    }
+
+    return Column(children: <Widget>[
+      Expanded(child:
+        SingleChildScrollView(scrollDirection: Axis.vertical, child:
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: content,),
+        ),
+      ),
+      _buildMembershipRequest(),
+      _buildCancelMembershipRequest(),
+    ],);
+  }
+
+  Widget _buildGroupContent() {
     Widget content;
     if (_isMemberOrAdmin) {
       content = TabBarView(
@@ -353,16 +372,7 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
       );
     }
     else {
-      content = SingleChildScrollView(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          _buildAbout(),
-          _buildPrivacyDescription(),
-          _buildAdmins(),
-          if (_isPublic && CollectionUtils.isNotEmpty(_groupEvents))
-            _GroupEventsContent(),
-          _buildResearchProjectMembershipRequest(),
-        ]),
-      );
+      //non-member
     }
 
     return NestedScrollView(
@@ -379,9 +389,7 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
                     pinned: true,
                     expandedHeight: _groupHeaderHeight,
                     flexibleSpace: _groupHeader,
-                    bottom: _isMemberOrAdmin ? TextTabBar(tabs: _buildTabs(), labelStyle: Styles().textStyles.getTextStyle('widget.heading.medium_small'),
-                        labelPadding: const EdgeInsets.symmetric(horizontal: 6.0,), controller: _tabController,
-                        backgroundColor: Styles().colors.fillColorPrimary, isScrollable: false, onTap: _onTabChanged) : null,
+                    bottom: _buildTabs(),
                   )
               )
           ),
@@ -766,10 +774,9 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: contentList);
   }
 
-  /*
-  Widget _buildTabs() {
-    if(CollectionUtils.isEmpty(_tabs))
-      return Container();
+  PreferredSizeWidget? _buildTabs() {
+    if (CollectionUtils.isEmpty(_tabs) || !_isMemberOrAdmin)
+      return null;
 
     List<Widget> tabs = [];
     for (_DetailTab tab in _tabs!) {
@@ -791,79 +798,26 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
           title = Localization().getStringEx("panel.group_detail.button.about.title", 'About');
           break;
         case _DetailTab.Scheduled:
-          title = Localization().getStringEx("", 'Scheduled'); //localize
-          break;
-      }
-
-      Tab tabWidget = Tab(
-          text: title,
-          height: 35,
-      );
-      tabs.add(tabWidget);
-    }
-
-    // Widget leaveButton = GestureDetector(
-    //     onTap: _onTapLeave,
-    //     child: Padding(
-    //         padding: EdgeInsets.only(left: 24, top: 10, bottom: 10),
-    //         child: Text(Localization().getStringEx("panel.group_detail.button.leave.title", 'Leave'),
-    //             style: Styles().textStyles.getTextStyle('panel.group.button.leave.title')
-    //         )));
-    //
-    // return Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16), child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-    //   SingleChildScrollView(scrollDirection: Axis.horizontal, child: Row(children: tabs)),
-    //   Visibility(visible: _canLeaveGroup, child: Padding(padding: EdgeInsets.only(top: 5), child: Row(children: [Expanded(child: Container()), leaveButton])))
-    // ]));
-
-    if(_tabController == null || _tabController!.length != tabs.length){
-      _tabController = TabController(length: tabs.length, vsync: this);
-    }
-
-    return Container(color: Colors.white, child:
-      TabBar(
-        tabs: tabs,
-        indicatorColor: Color(0xffF15C22),
-        controller: _tabController,
-        onTap:(index) => _onTab(_tabAtIndex(index)),
-        indicatorSize: TabBarIndicatorSize.tab,
-        // indicatorPadding: EdgeInsets.zero,
-        labelPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 0.0),
-        labelStyle: TextStyle(fontSize: 16.0, height: 1.0),
-        indicatorWeight: 4,
-        tabAlignment: TabAlignment.fill,
-    ));
-  }
-  */
-
-  List<Widget> _buildTabs() {
-    List<Widget> tabs = [];
-    for (_DetailTab tab in _DetailTab.values) {
-      String title;
-      switch (tab) {
-        case _DetailTab.Events:
-          title = Localization().getStringEx("panel.group_detail.button.events.title", 'Events');
-          break;
-        case _DetailTab.Posts:
-          title = Localization().getStringEx("panel.group_detail.button.posts.title", 'Posts');
-          break;
-        case _DetailTab.Messages:
-          title = Localization().getStringEx("panel.group_detail.button.messages.title", 'Messages');
-          break;
-        case _DetailTab.Polls:
-          title = Localization().getStringEx("panel.group_detail.button.polls.title", 'Polls');
-          break;
-        case _DetailTab.About:
-          title = Localization().getStringEx("panel.group_detail.button.about.title", 'About');
-          break;
-        case _DetailTab.Scheduled:
-          title = Localization().getStringEx("", 'Scheduled'); //localize
+          title = Localization().getStringEx("panel.group_detail.button.scheduled.title", 'Scheduled'); //localize
           break;
       }
 
       tabs.add(TextTabButton(title: title));
     }
 
-    return tabs;
+    if(_tabController == null || _tabController!.length != tabs.length){
+      _tabController = TabController(length: tabs.length, vsync: this);
+    }
+
+    return TextTabBar(
+      tabs: tabs,
+      labelStyle: Styles().textStyles.getTextStyle('widget.heading.medium_small'),
+      labelPadding: const EdgeInsets.symmetric(horizontal: 6.0,),
+      controller: _tabController,
+      backgroundColor: Styles().colors.fillColorPrimary,
+      isScrollable: false,
+      onTap: (index) => _onTab(_tabAtIndex(index)),
+    );
   }
 
   Widget _buildViewPager(){
@@ -1450,40 +1404,12 @@ class _GroupDetailPanelState extends State<GroupDetailPanel> with TickerProvider
         });
   }
 
-  // void _onTab(_DetailTab? tab) {
-  //   Analytics().logSelect(target: "Tab: $tab", attributes: _group?.analyticsAttributes);
-  //   if (tab != null /*&& _currentTab != tab*/) {
-  //       _currentTab = tab;
-  //
-  //     _pageController?.animateToPage(_indexOfTab(tab), duration: Duration(milliseconds: _animationDurationInMilliSeconds), curve: Curves.linear);
-  //   }
-  // }
-
-  void _onTabChanged(int index) {
-    _DetailTab tab = _DetailTab.values[_tabController.index];
+  void _onTab(_DetailTab? tab) {
     Analytics().logSelect(target: "Tab: $tab", attributes: _group?.analyticsAttributes);
-    if (!_tabController.indexIsChanging && _currentTab.index != _tabController.index) {
-      setState(() {
+    if (tab != null /*&& _currentTab != tab*/) {
         _currentTab = tab;
-      });
 
-      // switch (_currentTab) {
-      //   case _DetailTab.Posts:
-      //     if (CollectionUtils.isNotEmpty(_posts)) {
-      //       _scheduleLastPostScroll();
-      //     }
-      //     break;
-      //   case _DetailTab.Messages:
-      //     if (CollectionUtils.isNotEmpty(_messages)) {
-      //       _scheduleLastMessageScroll();
-      //     }
-      //     break;
-      //   case _DetailTab.Polls:
-      //     _schedulePollsScroll();
-      //     break;
-      //   default:
-      //     break;
-      // }
+      _pageController?.animateToPage(_indexOfTab(tab), duration: Duration(milliseconds: _animationDurationInMilliSeconds), curve: Curves.linear);
     }
   }
 
