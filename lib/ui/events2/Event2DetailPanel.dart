32069a2c27309c2942e6fb2c7427f44a93a2a7af
manavmodi <manav@rokmetro.com>
2025-02-14 12:10:12 -0500

    Fix bugs

--- lib/ui/events2/Event2DetailPanel.dart
+++ lib/ui/events2/Event2DetailPanel.dart
@@ -314,7 +314,7 @@ class _Event2DetailPanelState extends Event2Selector2State<Event2DetailPanel> im
       ...?_privacyDetailWidget,
       ...?_publishedDetailWidget,
       ...?_superEventDetailWidget,
-      ...?_promoteButton,
+      if (!kIsWeb) ...?_promoteButton,
       ...?_addToCalendarButton,
       ...?_adminCommandsButton,
       ...?_attendanceDetailWidget,
